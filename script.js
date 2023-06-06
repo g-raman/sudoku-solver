cf808a06cfaefa923c5da79116ef4934c3367a06
Raman Gupta <gr.gupta.raman@gmail.com>
2023-06-06 15:42:42 -0400

    Add function to convert input values to 2D array

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
-'use strict';
+"use strict";
 
+function boardToArray() {
+  const board = document.querySelectorAll(".board__cell");
+  let arr = [[], [], [], [], [], [], [], [], []];
+
+  for (let cellIndex = 0; cellIndex < board.length; cellIndex++) {
+    const row = Math.floor(cellIndex / 9);
+    const col = cellIndex % 9;
+
+    arr[row][col] = board[cellIndex].value;
+  }
+
+  return arr;
+}
